d7cd0451d7b92fb65e4c834aa1e4f234950af0a8
marcosvor <marcos_ribeiro@outlook.com>
2020-02-09 23:06:50 -0300

    Fix do controller publico para mostrar pagina inicial

--- src/controller/PublicController.java
+++ src/controller/PublicController.java
@@ -31,9 +31,9 @@ public class PublicController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		//RequestDispatcher resposta = request.getRequestDispatcher("index.html");
-		//resposta.forward(request, response);
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher resposta = request.getRequestDispatcher("index.html");
+		resposta.forward(request, response);
 	}
 
 	/**
